9a118726c8352b21064c7b50ef6918641f7c2ec0
Deepanshu <deepanshu@gojek.com>
2022-05-09 19:26:32 +0530

    Remove title from layout

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -34,9 +34,7 @@ const HomepageHeader = () => {
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+    <Layout>
       <HomepageHeader />
       <main>
         <Container className="textSection wrapper" background="dark">
